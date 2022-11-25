3adbf63c6c0c6c22c57e781e09fc3f5d0a001672
adimeble <111139813+adimeble@users.noreply.github.com>
2022-11-25 18:52:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 function openNav()
 {
   document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("arrow-down").style.display = "none";
+  document.getElementById("myBtn").style.display = "none";
 }
   
 /* Close/hide the sidenav */
 function closeNav()
 {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("arrow-down").style.display = "";
+  document.getElementById("myBtn").style.display = "";
 }
 
 // Get the button
